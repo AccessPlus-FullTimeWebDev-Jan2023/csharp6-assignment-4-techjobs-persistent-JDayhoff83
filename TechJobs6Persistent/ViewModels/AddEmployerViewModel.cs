165880de46d1ef01815efaf6f737a0d041b5060c
Jacob Dayhoff <dayhoff@gmail.com>
2023-04-24 10:18:21 -0500

    Work through task2Views

---
+++ TechJobs6Persistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name of Employer is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Employer Name should be between 3 and 50 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Location of Employer is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Location should be between 3 and 50 characters.")]
+        public string? Location { get; set; }
+    }
+}
--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -4,6 +4,8 @@
 
 --Part 1: List the columns and their data types in the Jobs table.
 
+--Jobs Table Columns: Id:int, Name:string, EmployerId:int
+
 --Part 2: Write a query to list the names of the employers in St. Louis City.
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
